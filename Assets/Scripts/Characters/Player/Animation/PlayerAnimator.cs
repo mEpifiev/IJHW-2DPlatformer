4cf693efea54d62fc9c9060c302644d945c7a320
Marlen Epifiev <epifiev@gmail.com>
2025-06-30 16:32:18 +0300

    Update PlayerAnimator.cs

--- Assets/Scripts/Characters/Player/Animation/PlayerAnimator.cs
+++ Assets/Scripts/Characters/Player/Animation/PlayerAnimator.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour
 {
     private readonly int ParamsRun = Animator.StringToHash("IsRun");
+    private readonly int ParamsAttack = Animator.StringToHash("IsAttack");
+    private readonly int ParamsDeath = Animator.StringToHash("Death");
 
     private Animator _animator;
 
@@ -20,5 +22,15 @@ public class PlayerAnimator : MonoBehaviour
     public void SetRunAnimation()
     {
         _animator.SetBool(ParamsRun, true);
+    }
+
+    public void SetAttackAnimation()
+    {
+        _animator.SetTrigger(ParamsAttack);
+    }
+
+    public void SetDeathAnimation()
+    {
+        _animator.SetTrigger(ParamsDeath);
     }
 }
