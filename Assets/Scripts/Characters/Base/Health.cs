ead29a14b59cfa169dae077e7668bd28acd2f384
Marlen Epifiev <epifiev@gmail.com>
2025-06-30 16:17:23 +0300

    Health System

--- Assets/Scripts/Characters/Base/Health.cs
+++ Assets/Scripts/Characters/Base/Health.cs
@@ -1,5 +1,51 @@
+using System;
 using UnityEngine;
 
-public class Health : MonoBehaviour
+public class Health : MonoBehaviour, IDamageable
 {
+    [SerializeField] private float _maxHealth;
+
+    private float _currentHealth;
+
+    public event Action<float> Changed;
+    public event Action Died;
+
+    private void Awake()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    private void OnValidate()
+    {
+        if (_maxHealth <= 0)
+            _maxHealth = 1;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (damage <= 0)
+            return;
+
+        _currentHealth = Mathf.Max(0, _currentHealth - damage);
+
+        Changed?.Invoke(_currentHealth);
+
+        if (_currentHealth <= 0)
+            Die();
+    }
+
+    public void Heal(float amount)
+    {
+        if (amount <= 0)
+            return;
+
+        _currentHealth = Mathf.Min(_maxHealth, _currentHealth + amount);
+        Debug.Log(_currentHealth);
+        Changed?.Invoke(_currentHealth);
+    }
+
+    private void Die()
+    {
+        Died?.Invoke();
+    }
 }
